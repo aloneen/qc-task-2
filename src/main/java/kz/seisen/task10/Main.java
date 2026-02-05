b70e8a2c0b6c1ebf9fb7e3621654adf28fa46e1a
aloneen <seisenbek2005@mail.ru>
2026-02-05 16:14:11 +0500

    feat: added .env file

--- src/main/java/kz/seisen/task10/Main.java
+++ src/main/java/kz/seisen/task10/Main.java
@@ -1,5 +1,6 @@
 package kz.seisen.task10;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.nio.file.Path;
 
 public class Main {
@@ -7,7 +8,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         // yes "some long line of text for testing" | head -n 50000000 > bigfile.txt
-        Path file = Path.of("/Users/diasseisenbek/Desktop/qazcode/tests/bigfile.txt");
+
+        Dotenv dotenv = Dotenv.load();
+        String filePath = dotenv.get("INPUT_FILE");
+
+        if (filePath == null || filePath.isBlank()) {
+            System.out.println("INPUT_FILE env variable is not set");
+            return;
+        }
+
+        Path file = Path.of(filePath);
 
         Result result = FileProcessor.process(file);
 
